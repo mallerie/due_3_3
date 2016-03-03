7265fea960bc71b30afb7b5245d0362bc5a735cb
Mallerie Shirley <mallerie@gmail.com>
2016-03-03 07:45:37 -0500

    Made it to problem three
    Got stuck on problem three... I think I may have to use the .filter function, but I can't figure out the proper syntax...

--- excercise-2.js
+++ excercise-2.js
@@ -2,25 +2,60 @@
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
 // ---------------------
 
-function max(){
-    //...
-}
+function max(paramOne, paramTwo) {
+  if (Number(paramOne) > Number(paramTwo))
+    return (paramOne);
+            else return (paramTwo);
+  }
 
 // ---------------------
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
 // ---------------------
 
-function maxOfThree(){
-    //...
-}
+function maxOfThree(one, two, three) {
+ if (Number(one) > Number(two) && Number(one) > Number(three)){
+      return one;
+      } else if (Number(two) > Number(three)){
+        return two;
+          } else {
+            return three;
+              }}
 
 // ---------------------
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 // ---------------------
 
-function isVowel(char){
-    //...
-}
+var allVowels = ['a', 'e', 'i', 'o', 'u'];
+
+allVowels.forEach function isVowel(char)  {
+  if (char.length === 1 && char === function())  {
+    return true; 
+      } else {
+        return false;
+    }}
+
+
+var allVowels = ['a', 'e', 'i', 'o', 'u'];          
+
+function isVowel(char) {
+  if (char === allVowels.forEach(function(vowel)  { 
+    if (vowel === char) {
+        return true;
+     } else {
+        return false;
+          }}
+
+var allVowels = ['a', 'e', 'i', 'o', 'u'];          
+
+allVowels.filter(function(char) {
+  var 
+}          
+
+
+
+// function isLetter(character) {
+//   return character >= 'a' && character <= 'z';
+// }
 
 // ---------------------
 // Write a function translate() that will translate a text into "rÃ¶varsprÃ¥ket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
